f6dadf1676729ec6d4d494409aad2d7d8cb13d56
gracehannon <hannonga@g.cofc.edu>
2024-04-24 13:56:33 -0400

    update

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,8 @@
-<script>
-	export let name;
-</script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<h1>Svelte!</h1>
+	<p>Svelte is a JavaScript framework that is primarily used to create applications.</p>
+	<p>This is an example of a Svelte page.</p>
 </main>
 
 <style>
@@ -19,6 +18,12 @@
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
+	}
+
+	p {
+		font-size: 1.2em;
+		color: #333; 
+		padding: 0.5em 0; 
 	}
 
 	@media (min-width: 640px) {
